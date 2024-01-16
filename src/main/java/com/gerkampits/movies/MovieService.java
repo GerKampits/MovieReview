bb36823a3dea6a99dfd3af168e5ef91ff1f78e99
GerKampits <gerkampitsis@gmail.com>
2024-01-16 02:13:00 +0200

    initialize project

---
+++ src/main/java/com/gerkampits/movies/MovieService.java
@@ -0,0 +1,33 @@
+package com.gerkampits.movies;
+
+import jakarta.websocket.server.PathParam;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MovieService {
+    private MovieRepository movieRepository;
+
+    @Autowired
+    public MovieService(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
+    public List<Movie> allMovies() {
+        return movieRepository.findAll();
+    }
+
+
+    public Optional<Movie> singleMovie(String imdbId) {
+        return movieRepository.findMovieByImdbId(imdbId);
+    }
+}
